6d220361bd1d5be0c6626cd51ff93e63eb52fd4a
Akash Karmacharya <akashkarmacharya101@gmail.com>
2024-07-22 01:02:51 +0200

    update changes

--- config.py
+++ config.py
@@ -9,8 +9,3 @@ class Config:
     SECRET_FOR_JWT = os.getenv('SECRET_FOR_JWT', 'JWT123')
     SECRET_FOR_QR = os.getenv('SECRET_FOR_QR', 'QR123')
     ENDPOINT = os.getenv('ENDPOINT', 'http://localhost:7000')
-
-# Debug prints to check if values are being loaded
-print("SECRET_FOR_JWT:", Config.SECRET_FOR_JWT)
-print("SECRET_FOR_QR:", Config.SECRET_FOR_QR)
-print("ENDPOINT:", Config.ENDPOINT)
